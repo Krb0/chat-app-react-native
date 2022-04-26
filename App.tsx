a2d497f977ef7f985f30efe4d041cbc8dc824c39
Krb0 <maurodavidcaceres@gmail.com>
2022-04-26 04:31:18 -0300

    auth context added

--- App.tsx
+++ App.tsx
@@ -5,26 +5,51 @@ import { createStackNavigator } from "@react-navigation/stack";
 import ChatScreen from "./screens/ChatScreen";
 import LoginScreen from "./screens/LoginScreen";
 import SignUpScreen from "./screens/SignUpScreen";
+import HomeScreen from "./screens/HomeScreen";
+import { createContext, useContext, useState } from "react";
 
 const Stack = createStackNavigator();
+const AuthenticatedUserContext = createContext({});
+const AuthenticatedUserProvider = ({ children }: any) => {
+  const [user, setUser] = useState(null);
+  return (
+    <AuthenticatedUserContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthenticatedUserContext.Provider>
+  );
+};
 
 const ChatStack = () => {
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Stack.Screen name="SignUp" component={SignUpScreen} key="SignUp" />
-      <Stack.Screen name="Login" component={LoginScreen} key="Login" />
+    <Stack.Navigator defaultScreenOptions={HomeScreen}>
+      <Stack.Screen name="HomeScreen" component={HomeScreen} key="Home" />
+      <Stack.Screen name="ChatScreen" component={ChatScreen} key="Chat" />
     </Stack.Navigator>
   );
 };
-const RootNavigator = () => (
-  <NavigationContainer>
-    <ChatStack />
-  </NavigationContainer>
-);
+const AuthStack = () => {
+  <Stack.Navigator defaultScreenOptions={LoginScreen}>
+    <Stack.Screen
+      name="LoginScreen"
+      component={LoginScreen}
+      key="LoginScreen"
+    />
+    <Stack.Screen
+      name="SignUpScreen"
+      component={SignUpScreen}
+      key="SignUpScreen"
+    />
+  </Stack.Navigator>;
+};
+
+const RootNavigator = () => {
+  const { user, setUser }: any = useContext(AuthenticatedUserContext);
+  return (
+    <NavigationContainer>
+      <ChatStack />
+    </NavigationContainer>
+  );
+};
 
 export default function App() {
   return <RootNavigator />;
